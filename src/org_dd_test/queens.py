c60f85250aa5e0672e2798cf45b833e160bceb0c
daidan <daidan@10.80.41.31>
2015-06-01 23:25:55 +0800

    file（6.1.23:25）

--- src/org_dd_test/queens.py
+++ src/org_dd_test/queens.py
@@ -2,6 +2,7 @@
 @author: LENOVO
 '''
 import random
+import os
 
 def conflict(state,nextX):
   nextY=len(state)
@@ -25,8 +26,37 @@ def prettyprint(solution):
      for pos in solution:
          print line(pos)
          
-         
-                                            
+
+def make_text_file():
+    a=open('test.txt','w')
+    a.write("This is how you create a new text file")
+    a.close
+    print ("Creat Success")
+
+def add_some_text():
+    a=open('test.txt',"a")
+    a.write("Here add hy love dd")
+    a.close
+    
+def readText():
+    a=open('test.txt',"r")
+    print(a.read())
+    
+def lines_lengths():    
+    a=open("test.txt","r")
+    text=a.readlines()
+    for line in text:
+     print(line)
+     
+def deleteFile():   
+    a=open("test.txt","r")  
+   #  del a
+    # a.close
     
 if __name__ == '__main__':
+    
+     #make_text_file()
+    #add_some_text()
+    #readText()
+   # lines_lengths()
         prettyprint(random.choice(list(queens(8))))
